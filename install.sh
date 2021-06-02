eacafee30d5a874736305c08f555a87afec27168
John Romberg <john@techtutorkids.com>
2021-06-02 15:38:59 -0600

    Final touches

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ echo "You need to install sudo to run this script."
 exit 1
 fi
 
-read -p "Warning: This install script uses sudo. You might want to read the code before running. Do you want to continue? [Y/n]: " onward
+read -p "Warning: This install script uses sudo. You might want to read the code before running. Do you wish to continue? [Y/n]: " onward
 
 if [[ $onward == n || $onward == N ]]; then
 echo "Ok."
